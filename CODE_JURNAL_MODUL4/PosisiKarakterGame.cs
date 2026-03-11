7709f26cd8d1ae036e9b136eb10347f5e8ac21e7
ariel <fadillah0777@gmail.com>
2026-03-11 22:49:27 +0800

    menambahkan class PosisiKarakterGame

---
+++ CODE_JURNAL_MODUL4/PosisiKarakterGame.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CODE_JURNAL_MODUL4
+{
+    class PosisiKarakterGame
+    {
+        // Enum digunakan untuk menentukan kemungkinan state karakter
+        enum State
+        {
+            Berdiri,
+            Tengkurap,
+            Terbang,
+            Jongkok
+        }
+
+        // State awal karakter adalah berdiri
+        private State currentState = State.Berdiri;
+
+        // Method untuk mengubah state karakter
+        public void UbahState(State newState)
+        {
+            // Mengecek perubahan state
+            if (newState == State.Berdiri)
+            {
+                Console.WriteLine("posisi standby");
+            }
+
+            if (newState == State.Tengkurap)
+            {
+                Console.WriteLine("posisi istirahat");
+            }
+
+            // Mengubah state karakter
+            currentState = newState;
+        }
+
+        // Method untuk mensimulasikan perubahan state
+        public void Simulasi()
+        {
+            // Simulasi perubahan state karakter
+
+            UbahState(State.Berdiri);
+            UbahState(State.Tengkurap);
+            UbahState(State.Terbang);
+            UbahState(State.Jongkok);
+        }
+    }
+}
--- CODE_JURNAL_MODUL4/Program.cs
+++ CODE_JURNAL_MODUL4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CODE_JURNAL_MODUL4;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membuat objek dari class KodeBuah
+        KodeBuah kodeBuah = new KodeBuah();
+
+        // Meminta user memasukkan nama buah
+        Console.Write("Masukkan nama buah: ");
+        string buah = Console.ReadLine();
+
+        // Memanggil method getKodeBuah untuk mencari kode buah
+        string kode = kodeBuah.getKodeBuah(buah);
+
+        // Menampilkan hasil kode buah
+        Console.WriteLine("Kode buah: " + kode);
+
+
+        Console.WriteLine("\nSimulasi Posisi Karakter Game");
+
+        // Membuat objek dari class PosisiKarakterGame
+        PosisiKarakterGame karakter = new PosisiKarakterGame();
+
+        // Memanggil simulasi perubahan state
+        karakter.Simulasi();
+    }
+}
